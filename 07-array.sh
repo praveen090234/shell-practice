8cdb9d8d09b1206ca8af95d874672eebab83ee7a
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-19 11:50:29 -0500

    arrays

--- 07-array.sh
+++ 07-array.sh
@@ -2,7 +2,7 @@
 
 MOVIES=("court" "HIT3" "thandel" "pushpa2")
 
-echo "first movie is : ${MOVIES[0]}
+echo "first movie is : ${MOVIES[0]}"
 echo "last movie is : ${MOVIES[3]}"
 
 echo "all movies are : ${MOVIES[@]}"
