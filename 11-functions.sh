f1d38f6c39e17afc1073d72b14288129688b7d4f
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-20 12:49:00 -0500

    if-condition

--- 11-functions.sh
+++ 11-functions.sh
@@ -11,20 +11,49 @@ else
      # exit form the script 
 fi
 
-dnf list installed mysql #if ecit code is not equal to 0 we are going to install my sql 
-                        # if exit code is equal to 0 nothing to do
+Validate () {
+if [ $1 -eq 0 ]
+    then
+        echo "installing $2 is ....... success"
+    else 
+        echo "installing $2 is ..... failure"
+        exit 1
+    fi
+
+} 
+
+
+dnf list installed mysql 
 
 if [ $? -ne 0 ]
 then
     echo "mysql is not installed.....going to install it"
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "installing mysql is ....... success"
-    else 
-        echo "installing mysql is ..... failure"
-        exit 1
-    fi
+    $Validate $1 "mysql"
+    
 else
     echo "mysql is already installed... nothing to do"
+fi
+
+dnf list installed python3 
+
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed.....going to install it"
+    dnf install python3 -y
+    $Validate $1 "python"
+else
+    echo "python3 is already installed... nothing to do"
+fi
+
+
+dnf list installed nginx 
+
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed.....going to install it"
+    dnf install nginx -y
+   $Validate $1 "nginx" 
+else
+    echo "nginx is already installed... nothing to do"
 fi
