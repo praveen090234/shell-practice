7e5f09f8e185ac00826c9df3d022a384e34e8037
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-20 11:20:48 -0500

    if-condition

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -4,8 +4,18 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error:: this user is not running woth root access"
+    echo "Error:: this user is not running with root access"
     exit 1
 else
     echo "User is running with root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "installing mysql is..... success"
+else
+    echo "installing mysql is..... failure"
+    exit 1
 fi
