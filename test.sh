8fa7e4b4137bab509d2d3769e7fb0e6b82ed2005
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-22 11:55:43 -0500

    logs

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
 #1/bin/bash
 
-MOVIES=("pushpa" "court" "thandel" "test")
-
-echo "first movie is  ${MOVIES[0]}"
-echo "last movie is ${MOVIES[3]}"
-
-echo "all the movies are ${MOVIES[@]}"
+echo "script executed at: $(date)"
