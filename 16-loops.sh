64c30f086d58dd49adf649d4843cdf624d4fafbd
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-22 11:13:28 -0500

    logs

---
+++ 16-loops.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-practice-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python3" "nginx" "httpd")
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: $N This user is not running with root access" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G This user is running with root access $N" | tee -a $LOG_FILE
+     # exit form the script 
+fi
+
+VALIDATE(){
+if [ $1 -eq 0 ]
+    then
+        echo -e "installing $2 is ....... $G success $N" | tee -a $LOG_FILE
+    else 
+        echo -e "installing $2 is ..... $R failure $n" | tee -a $LOG_FILE
+        exit 1
+    fi
+} 
+
+
+for packages {PACKAGES[@]}
+do
+  dnf list installed $packages  &>>$LOG_FILE
+  if [ $? -ne 0 ]
+then
+    echo -e "$Y $packages is not installed.....going to install it $N" | tee -a $LOG_FILE
+    dnf install $packages -y &>>$LOG_FILE
+    VALIDATE $? "$packages"
+    
+else
+    echo -e "$Y $packages is already installed... nothing to do $N" | tee -a $LOG_FILE
+fi
+
+
+
+
+
+# dnf list installed mysql  &>>$LOG_FILE
+
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$Y mysql is not installed.....going to install it $N" | tee -a $LOG_FILE
+#     dnf install mysql -y &>>$LOG_FILE
+#     VALIDATE $? "mysql"
+    
+# else
+#     echo -e "$Y mysql is already installed... nothing to do $N" | tee -a $LOG_FILE
+# fi
+
+
+
+# dnf list installed python3 &>>$LOG_FILE
+
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$Y python3 is not installed.....going to install it" | tee -a $LOG_FILE
+#     dnf install python3 -y &>>$LOG_FILE
+#     VALIDATE $? "python"
+# else
+#     echo -e "$Y python3 is already installed... nothing to do $N" | tee -a $LOG_FILE
+# fi
+
+
+# dnf list installed nginx &>>$LOG_FILE
+
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$Y nginx is not installed.....going to install it $N" | tee -a $LOG_FILE
+#     dnf install nginx -y &>>$LOG_FILE
+#     VALIDATE $? "nginx" 
+# else
+#     echo -e  "$Y nginx is already installed... nothing to do $N" | tee -a $LOG_FILE
+# fi
