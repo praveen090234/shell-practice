339d27be91bf9def04fae3535de347cb4e866619
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-22 16:46:53 -0500

    logs

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]
 then 
-     echo "this user have root access"
+     echo "this user don't have root access"
 else
-     echo "this user need root access"
+     echo "this user have root access"
 fi
 
 
