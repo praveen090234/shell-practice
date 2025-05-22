f9ea3f24534af5bb5a6d6ff5902b8c398af7a5dd
Praveen Kumar Nasika <pranasik@publicisgroupe.net>
2025-05-22 17:15:31 -0500

    logs

--- test.sh
+++ test.sh
@@ -9,5 +9,19 @@ else
      echo "this user have root access"
 fi
 
+dnf list installed mysql
+if [ $? -ne 0]
+then
+    echo "Error: my sql is not installed...installing mysql"
+    dnf install mysql -y
+    if [ $? -eq 0]
+    then
+        echo "mysql installation is successful"
+    else
+       echo "mysql installation is failure"
+    fi
+else
+    echo "mysql is installed...nothing to do"
+
 
 
